fa197b11758e40be8037e500ccd5d5606e94fc0d
z1a2q3x1s2w3 <z1a2q3x1s2w3@proton.me>
2024-02-20 19:55:59 +0000

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+fn main() {
+	println!("Hello, world!");
+	
+	// Features
+	// Use checkip.amazonaws.com to get WAN IP address
+	// Use CloudFlare API to update DNS record
+	// Include logging functionality
+	// Include command line argument parsing
+		// Switches for domain name and API token
+	
+	// Outline:
+	// wan_address = get_wan_address()
+	// dns_address = get_dns_address()
+	// if wan_address != dns_address
+		// update_dns_record
+}
