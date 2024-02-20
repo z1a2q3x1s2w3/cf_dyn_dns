0a29701bc9f422bbec9d4f00cc4c7e405208682c
z1a2q3x1s2w3 <z1a2q3x1s2w3@proton.me>
2024-02-20 20:07:10 +0000

    Started working on get_wan_address function

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,21 @@
+
+// Features
+// Use checkip.amazonaws.com to get WAN IP address
+// Use CloudFlare API to update DNS record
+// Include logging functionality
+// Include command line argument parsing
+	// Switches for domain name and API token
+
+// Outline:
+// wan_address = get_wan_address()
+// dns_address = get_dns_address()
+// if wan_address != dns_address
+	// update_dns_record
+
+fn get_wan_address() -> String {
+	String::from("1.2.3.4")
+}
+
 fn main() {
-	println!("Hello, world!");
-	
-	// Features
-	// Use checkip.amazonaws.com to get WAN IP address
-	// Use CloudFlare API to update DNS record
-	// Include logging functionality
-	// Include command line argument parsing
-		// Switches for domain name and API token
-	
-	// Outline:
-	// wan_address = get_wan_address()
-	// dns_address = get_dns_address()
-	// if wan_address != dns_address
-		// update_dns_record
+	println!("{}", get_wan_address());
 }
