660541d0c9ec29ad4b619af6ccc5e6930b13ba82
z1a2q3x1s2w3 <z1a2q3x1s2w3@proton.me>
2024-02-21 22:17:42 +0000

    Added functions to get WAN and DNS IP. Also added argument parser.

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,6 @@
-
+use error_chain::error_chain;
+use clap::{arg, command};
+use std::net::ToSocketAddrs;
 // Features
 // Use checkip.amazonaws.com to get WAN IP address
 // Use CloudFlare API to update DNS record
@@ -12,10 +14,44 @@
 // if wan_address != dns_address
 	// update_dns_record
 
-fn get_wan_address() -> String {
-	String::from("1.2.3.4")
+error_chain! {
+	foreign_links {
+		Io(std::io::Error);
+		HttpRequest(reqwest::Error);
+	}
 }
 
-fn main() {
-	println!("{}", get_wan_address());
+fn get_wan_address() -> Result<String> {
+	let ip_reflect_url = "https://checkip.amazonaws.com";
+	let body = reqwest::blocking::get(ip_reflect_url)?
+		.text()?;
+	Ok(body
+		.trim()
+		.to_string())
+}
+
+fn get_dns_address(domain: &str) -> Result<String> {
+	let socket_address = String::from(domain) + ":443";
+	let mut ip_addresses = socket_address.to_socket_addrs()?;
+	if let Some(addr) = ip_addresses.next() {
+		Ok(addr.ip().to_string())
+	} else {
+		Err("No IP address found for the domain".into())
+	}
+}
+
+fn main() -> Result<()> {
+	let matches = command!()
+//		.author("zaqxsw")
+//		.about("CloudFlare dynamic DNS")
+		.arg(arg!(-d --domain <URL> "The domain to be updated").required(true))
+		.get_matches();
+	
+	let wan_address = get_wan_address()?;
+	println!("WAN IP = {:?}", wan_address);
+	
+	let dns_address = get_dns_address(matches.get_one::<String>("domain").expect("required"))?;
+	println!("DNS IP = {:?}", dns_address);
+	
+	Ok(())
 }
