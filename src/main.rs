a73c6243e7130014ec48de40ab63dba713054219
z1a2q3x1s2w3 <z1a2q3x1s2w3@proton.me>
2024-02-23 18:27:24 +0000

    Removed comments

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,6 @@ use clap::{arg, command};
 use std::net::ToSocketAddrs;
 use serde_json::json;
 use serde_json::Value;
-// Features
-// Use checkip.amazonaws.com to get WAN IP address
-// Use CloudFlare API to update DNS record
-// Include logging functionality
-// Include command line argument parsing
-	// Switches for domain name and API token
-
-// Outline:
-// wan_address = get_wan_address()
-// dns_address = get_dns_address()
-// if wan_address != dns_address
-	// update_dns_record
 
 error_chain! {
 	foreign_links {
